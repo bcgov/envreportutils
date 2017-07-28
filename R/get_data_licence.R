ecf52237ccde7e15126be64b8a3cd958f69747c5
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2017-07-28 15:42:56 -0700

    license to licence

--- R/get_data_licence.R
+++ R/get_data_licence.R
@@ -12,7 +12,7 @@
 
 #' Returns a url, or a markdown or html-formatted link to one of several B.C. or Canadian licences
 #'
-#' @param license Which licence? One of:
+#' @param licence Which licence? One of:
 #' \itemize{
 #'   \item ogl-bc Open Government Licence - British Columbia
 #'   \item bc-crown B.C. Crown Copyright
@@ -27,18 +27,19 @@
 #' }
 #'
 #' @return character containing the desired output
+#' @aliases get_data_license
 #' @export
 #'
 #' @examples
-#' get_data_license("ogl-bc", "url")
+#' get_data_licence("ogl-bc", "url")
 #' 
-#' get_data_license("ogl-canada")
+#' get_data_licence("ogl-canada")
 #' 
-#' get_data_license("statscan", "html_link")
-get_data_license = function(license = c("ogl-bc", "bc-crown", "ogl-canada", "statscan"), 
+#' get_data_licence("statscan", "html_link")
+get_data_licence = function(licence = c("ogl-bc", "bc-crown", "ogl-canada", "statscan"), 
                             what = "md_link") {
   what <- match.arg(what, c("url", "md_link", "html_link"))
-  licenses <- list("ogl-bc" = c(name = "Open Government Licence - British Columbia", 
+  licences <- list("ogl-bc" = c(name = "Open Government Licence - British Columbia", 
                                 href = "http://www2.gov.bc.ca/gov/content?id=A519A56BC2BF44E4A008B33FCF527F61"),
                    "bc-crown" = c(name = "B.C. Crown Copyright", 
                                   href = "http://www2.gov.bc.ca/gov/content?id=1AAACC9C65754E4D89A118B875E0FBDA"),
@@ -46,9 +47,14 @@ get_data_license = function(license = c("ogl-bc", "bc-crown", "ogl-canada", "sta
                                     href = "http://open.canada.ca/en/open-government-licence-canada"),
                    "statscan" = c(name = "Statistics Canada Open Licence Agreement", 
                                   href = "http://www.statcan.gc.ca/eng/reference/licence"))
-  lic <- licenses[[license]]
+  lic <- licences[[licence]]
   switch(what, 
          url = lic["href"], 
          md_link = paste0("[", lic["name"], "](", lic["href"], ")"),
          html_link = paste0("<a href='", lic["href"], "'>", lic["name"], "</a>"))
 }
+
+
+#' @export
+get_data_license <- get_data_licence
+
