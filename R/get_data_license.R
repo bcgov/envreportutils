3706b784a6b510f4546c6307ad6f7ea6b1cde2c9
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2017-07-28 15:31:30 -0700

    fix spellings; write out OGL-BC

--- R/get_data_license.R
+++ R/get_data_license.R
@@ -10,20 +10,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-#' Returns a url, or a markdown or html-formatted link to one of several B.C. or Candadian licenses
+#' Returns a url, or a markdown or html-formatted link to one of several B.C. or Canadian licences
 #'
-#' @param license Which license? One of:
+#' @param license Which licence? One of:
 #' \itemize{
-#'   \item ogl-bc Open Government License - British Columbia
+#'   \item ogl-bc Open Government Licence - British Columbia
 #'   \item bc-crown B.C. Crown Copyright
-#'   \item ogl-canada Open Government License - Canada
+#'   \item ogl-canada Open Government Licence - Canada
 #'   \item statscan Statistics Canada Open Licence Agreement
 #' }
 #' @param what What do you want returned? One of
 #' \itemize{
 #'   \item md_link A markdown-formatted link (default)
 #'   \item html_link An HTML-formatted link
-#'   \item url The url of the license
+#'   \item url The url of the licence
 #' }
 #'
 #' @return character containing the desired output
@@ -38,11 +38,11 @@
 get_data_license = function(license = c("ogl-bc", "bc-crown", "ogl-canada", "statscan"), 
                             what = "md_link") {
   what <- match.arg(what, c("url", "md_link", "html_link"))
-  licenses <- list("ogl-bc" = c(name = "OGL-BC", 
+  licenses <- list("ogl-bc" = c(name = "Open Government Licence - British Columbia", 
                                 href = "http://www2.gov.bc.ca/gov/content?id=A519A56BC2BF44E4A008B33FCF527F61"),
                    "bc-crown" = c(name = "B.C. Crown Copyright", 
                                   href = "http://www2.gov.bc.ca/gov/content?id=1AAACC9C65754E4D89A118B875E0FBDA"),
-                   "ogl-canada" = c(name = "Open Government License - Canada", 
+                   "ogl-canada" = c(name = "Open Government Licence - Canada", 
                                     href = "http://open.canada.ca/en/open-government-licence-canada"),
                    "statscan" = c(name = "Statistics Canada Open Licence Agreement", 
                                   href = "http://www.statcan.gc.ca/eng/reference/licence"))
