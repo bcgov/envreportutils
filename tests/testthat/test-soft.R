6f97d6ba935de6566be41eb7352b2986ac989077
Andy Teucher <andy.teucher@gov.bc.ca>
2019-04-02 09:50:37 -0700

    Fix and add to tests for soft

--- tests/testthat/test-soft.R
+++ tests/testthat/test-soft.R
@@ -2,18 +2,26 @@ context("test soft")
 
 test_file <- tempfile()
 cat("test\n", file = test_file, append = TRUE)
+test_file2 <- tempfile()
+cat("test\n", file = test_file2, append = TRUE)
 test_dir <- tempdir()
-file.copy(test_file, test_dir, overwrite = TRUE)
+file.copy(c(test_file, test_file2), test_dir, overwrite = TRUE)
 
 test_that("zip_it works", {
   zipfile <- zip_it(test_dir)
   expect_true(file.exists(zipfile))
   expect_match(basename(zipfile), "^soft_send.+\\.zip$")
+  zipfile2 <- zip_it(test_dir, zipname = "foo.zip")
+  expect_true(file.exists(zipfile2))
+  expect_match(basename(zipfile2), "^foo\\.zip$")
+  zipfile3 <- zip_it(c(test_file, test_file2), zipname = "bar.zip")
+  expect_true(file.exists(zipfile3))
+  expect_match(basename(zipfile3), "^bar\\.zip$")
 })
 
 test_that("errors correctly", {
   expect_error(soft(file = "Asdfds190879"),
-               "file does not exist")
+               "files do not exist")
   expect_error(soft(file = test_file, email = "fooatgmaildotcom"),
                "You have entered an invalid email address")
   expect_error(soft(file = test_file, email = c("foo@bar.com", "bar@foo.com")),
