d609ed0e9e727f02fb32594b977e75b6d10316de
steffilazerte <steffi@steffi.ca>
2022-03-20 12:32:32 -0500

    Don't include file path in plain text

--- R/leaflet.R
+++ R/leaflet.R
@@ -233,8 +233,8 @@ popup_plot <- function(plot_loc, stn_id, metrics) {
   # Get only images that exist
   files <- list.files(
     plot_loc, 
-    pattern = paste0(stn_id, "_(", paste0(metrics, collapse = "|"), ").svg")) %>%
-    paste0("<img src = '", file.path(plot_loc, .), "'>") %>%
+    pattern = paste0(stn_id, "_(", paste0(metrics, collapse = "|"), ").svg"))
+  files <- paste0("<img src = '", file.path(plot_loc, files), "'>") %>%
     .[order(!stringr::str_detect(., metrics[1]))] # Arrange in order of metrics
   
   data.frame(popup_row_plot1 = popup_create_row(files[1]), 
